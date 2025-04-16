048520f3394dd2966dec01ab593d962ce0805ea7
Pieter Wigboldus <w3n@w3news.org>
2025-04-16 10:40:10 +0200

    Fix the expected formatted queries

--- test/github-issues/1738/queries.ts
+++ test/github-issues/1738/queries.ts
@@ -1,30 +1,21 @@
 export const CASE_1_UNFORMATTED = `SELECT * FROM tbl`
-export const CASE_1_FORMATTED = `SELECT
-  *
-FROM
-  tbl`
+export const CASE_1_FORMATTED = `SELECT *
+FROM tbl`
 
 export const CASE_2_UNFORMATTED = `SELECT tbl1.col1, tbl2.col2 FROM tbl1 INNER JOIN tbl2 ON tbl1.id = tbl2.id`
-export const CASE_2_FORMATTED = `SELECT
-  tbl1.col1,
-  tbl2.col2
-FROM
-  tbl1
-  INNER JOIN tbl2 ON tbl1.id = tbl2.id`
+export const CASE_2_FORMATTED = `SELECT tbl1.col1,
+    tbl2.col2
+FROM tbl1
+    INNER JOIN tbl2 ON tbl1.id = tbl2.id`
 
 export const CASE_3_UNFORMATTED = `SELECT col1, (SELECT col2 FROM tbl2 WHERE tbl2.id = tbl1.id) AS sub_col FROM tbl1`
-export const CASE_3_FORMATTED = `SELECT
-  col1,
-  (
-    SELECT
-      col2
-    FROM
-      tbl2
-    WHERE
-      tbl2.id = tbl1.id
-  ) AS sub_col
-FROM
-  tbl1`
+export const CASE_3_FORMATTED = `SELECT col1,
+    (
+        SELECT col2
+        FROM tbl2
+        WHERE tbl2.id = tbl1.id
+    ) AS sub_col
+FROM tbl1`
 
 export const FORMAT_SQL_TEST_CASES = [
     {
